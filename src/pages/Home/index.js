b39f059fff28e3c0a416ebd6402e044af35ad525
Gabriel West <gabe_west1@yahoo.com>
2018-04-13 23:52:37 -0500

    Home page includes a title, subtitle, and 2 links to adjust settings or play.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
 import React from "react"
+import styled from "styled-components"
+import Link from "../../components/Link"
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
+            <HomeView>
+                <Title>Whack A Idiot</Title>
+                <SubTitle>Family Fun</SubTitle>
 
-            </div>
+                <GameOptions>
+                    <Option><Link to="/whack-a-mole">Play</Link></Option>
+                    <Option><Link to="/settings">Settings</Link></Option>
+                </GameOptions>
+            </HomeView>
         )
     }
 }
+
+const Title = styled.h1``
+const SubTitle = styled.h3``
+const Option = styled.li``
+const GameOptions = styled.ul``
+const HomeView = styled.div`
+
+`
