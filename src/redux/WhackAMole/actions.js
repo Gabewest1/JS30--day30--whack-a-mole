ac2486de383b4b6b8c67870f330e98497eedf76a
Gabriel West <gabe_west1@yahoo.com>
2018-04-14 03:15:41 -0500

    created WackAMole actions: performMove, activateMole, deactivateMole, increaseScore, decreaseTimer and startGame.

---
+++ src/redux/WhackAMole/actions.js
@@ -0,0 +1,8 @@
+import { constants } from "./index"
+
+export const performMove = move => ({ type: constants.PERFORM_MOVE, payload: move })
+export const activateMole = mole => ({ type: constants.ACTIVATE_MOLE, payload: mole })
+export const deactivateMole = mole => ({ type: constants.DEACTIVATE_MOLE, payload: mole })
+export const increaseScore = () => ({ type: constants.INCREASE_SCORE })
+export const decreaseTimer = () => ({ type: constants.TICK })
+export const startGame = () => ({ type: constants.START_GAME })
