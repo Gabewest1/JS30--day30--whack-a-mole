513d180f41278ddf57d0697ad7ddb4a84d21c602
Gabriel West <gabe_west1@yahoo.com>
2018-04-14 03:18:18 -0500

    Updated WackAMole Page start the game, render the time score and 4 moles.

--- src/pages/WhackAMole/index.js
+++ src/pages/WhackAMole/index.js
@@ -1,11 +1,62 @@
 import React from "react"
+import styled from "styled-components"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
 
-export default class WhackAMole extends React.Component {
+import { 
+    actions as whackAMoleActions,
+    selectors as whackAMoleSelectors
+} from "../../redux/WhackAMole"
+
+class WhackAMole extends React.Component {
+    componentDidMount() {
+        this.props.startGame()
+    }
     render() {
+        const { performMove, score, time, moles } = this.props
+
+        const MolesView = moles.map((mole, i) => (
+            <MoleView 
+                key={ i }
+                mole={ mole }
+                onClick={ mole.isActive ? () => performMove(i): null } 
+            />
+        ))
+
         return (
-            <div>
-                Whack-A-Mole
-            </div>
+            <WhackAMoleView>
+                <TitleView>WhackAHoeeeee</TitleView>
+                <ScoreView>{ score }</ScoreView>
+                <TimeView>{ time }</TimeView>
+
+                { MolesView }
+
+            </WhackAMoleView>
         )
     }
 }
+
+const WhackAMoleView = styled.div``
+const TimeView = styled.div``
+const ScoreView = styled.div``
+const TitleView = styled.div``
+const MoleView = styled.div`
+    background: blue;
+    border-radius: 50%;
+    width: 55px;
+    height: 55px;
+`
+
+function mapStateToProps(state) {
+    return {
+        moles: whackAMoleSelectors.getMoles(state),
+        time: whackAMoleSelectors.getTime(state),
+        score: whackAMoleSelectors.getScore(state)
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ ...whackAMoleActions }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WhackAMole)
