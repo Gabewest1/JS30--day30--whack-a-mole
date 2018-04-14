ffe63ad78551e2c9b79b57d326a095ea9f6e47ee
Gabriel West <gabe_west1@yahoo.com>
2018-04-14 03:16:29 -0500

    Created WackAMole constants: start game, end game, reset game, perform move, tick, activate mole, deactivate mole, increase score.

---
+++ src/redux/WhackAMole/constants.js
@@ -0,0 +1,8 @@
+export const START_GAME = "START_GAME"
+export const END_GAME = "END_GAME"
+export const RESET_GAME = "RESET_GAME"
+export const PERFORM_MOVE = "PERFORM_MOVE"
+export const TICK = "TICK"
+export const ACTIVATE_MOLE = "ACTIVATE_MOLE"
+export const DEACTIVATE_MOLE = "DEACTIVATE_MOLE"
+export const INCREASE_SCORE = "INCREASE_SCORE"
