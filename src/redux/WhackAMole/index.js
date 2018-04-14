a21ec593f751db16610a608a07a51c9d8b8da7a9
Gabriel West <gabe_west1@yahoo.com>
2018-04-14 03:26:35 -0500

    Added index.js to WackAMole redux.

---
+++ src/redux/WhackAMole/index.js
@@ -0,0 +1,13 @@
+import * as actions from "./actions"
+import * as constants from "./constants"
+import * as selectors from "./selectors"
+import reducer from "./reducer"
+import sagas from "./sagas"
+
+export {
+    actions,
+    constants,
+    reducer,
+    selectors,
+    sagas
+}
