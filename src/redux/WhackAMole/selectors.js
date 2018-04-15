25090ce345523124bb399016b08975570913de60
Gabriel West <gabe_west1@yahoo.com>
2018-04-14 19:41:12 -0500

    Updated WhackAMole's getUnactiveMole(): Picks a random index, up to 6 times, and returns its index if the mole is unactive.

--- src/redux/WhackAMole/selectors.js
+++ src/redux/WhackAMole/selectors.js
@@ -7,3 +7,22 @@ export const getMoles = state => {
 export const getScore = state => state.whackAMole.score
 export const getTime = state => state.whackAMole.time / 1000
 export const isGameOver = state => getTime(state) <= 0
+export const getUnactiveMole = state => {
+    const moles = getMoles(state)
+    let maxTries = 6
+    let mole = -1
+
+    while (mole < 0 && maxTries > 0) {
+        let maxIndex = moles.length
+        let index = Math.floor(Math.random() * maxIndex)
+        let isUnactiveMole = moles[index].isActive === false
+
+        if (isUnactiveMole) {
+            mole = index
+        }
+
+        maxTries--
+    }
+
+    return mole
+}
