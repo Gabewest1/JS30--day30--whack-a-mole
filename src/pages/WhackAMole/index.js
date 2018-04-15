7b726840fb50bb3ef99d5d771c8cbbe16738acc7
Gabriel West <gabe_west1@yahoo.com>
2018-04-15 02:41:28 -0500

    Added images for a background, mole, and dirt. Updated WhackAMole page to use those assets.

--- src/pages/WhackAMole/index.js
+++ src/pages/WhackAMole/index.js
@@ -15,7 +15,7 @@ class WhackAMole extends React.Component {
     render() {
         const { performMove, score, time, moles } = this.props
 
-        const MolesView = moles.map((mole, i) => (
+        const Moles = moles.map((mole, i) => (
             <MoleView 
                 key={ i }
                 mole={ mole }
@@ -29,26 +29,58 @@ class WhackAMole extends React.Component {
                 <ScoreView>{ score }</ScoreView>
                 <TimeView>{ time }</TimeView>
 
-                { MolesView }
+                <MolesView>
+                    { Moles }
+                </MolesView>
 
             </WhackAMoleView>
         )
     }
 }
 
-const WhackAMoleView = styled.div``
+const WhackAMoleView = styled.div`
+    background: url(/background.png) no-repeat;
+    background-size: cover;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 const TimeView = styled.div``
 const ScoreView = styled.div``
 const TitleView = styled.div``
-const MoleView = styled.div`
-    background: blue;
-    border-radius: 50%;
-    width: 55px;
-    height: 55px;
-    opacity: ${({ mole }) => mole.isActive ? 1 : 0};
+const Mole = styled.img`
+    transform: translateY(${({ mole }) => mole.isActive ? "0%" : "130%"});
     transition: all .2s ease-in-out;
+    max-width: 100%;
 `
+const Dirt = styled.img`
+    max-width: 100%;
+    z-index: 1;
+    position: relative;
+    top: 14px;
+`
+const MoleView = ({ mole, ...props }) => (
+    <div { ...props } style={{ width: 150, position: "relative", overflow: "hidden" }}>
+        <Mole src="/mole_sprite.png" mole={ mole } />
+        <Dirt src="dirt_sprite.png" />
+    </div>
+)
+const MolesView = styled.div`
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    flex-wrap: wrap; 
+    justify-content: space-around;
 
+    > *:nth-child(1) {
+        top: 100px;
+    }
+    > *:nth-child(3) {
+        top: 66px;
+    }
+`
 function mapStateToProps(state) {
     return {
         moles: whackAMoleSelectors.getMoles(state),
