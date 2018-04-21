45cf7ab158d9feda35766d36590ce5d2bb13c3f2
Gabriel West <gabe_west1@yahoo.com>
2018-04-20 19:32:55 -0500

    Added Settings reducer to rootReducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,7 @@
 import { reducer as whackAMoleReducer } from "./redux/WhackAMole"
+import { reducer as settingsReducer } from "./redux/Settings"
 
 export default {
-    whackAMole: whackAMoleReducer
+    whackAMole: whackAMoleReducer,
+    settings: settingsReducer
 }
