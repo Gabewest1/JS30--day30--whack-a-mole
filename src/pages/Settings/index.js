13029a07b9414c9125cee33316c653bf70576585
Gabriel West <gabe_west1@yahoo.com>
2018-04-20 19:33:29 -0500

    Updated Settings Page to include a slider for the audio and buttons to set the difficulty.

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,11 +1,60 @@
 import React from "react"
+import styled from "styled-components"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
 
-export default class Settings extends React.Component {
+import { actions as settingsActions } from "../../redux/Settings"
+import { EASY, MEDIUM, HARD, VERY_HARD } from "../../constants"
+
+class Settings extends React.Component {
     render() {
+        const { setAudioLevel } = this.props
+
         return (
-            <div>
-                Settings
-            </div>
+            <SettingsView>
+                <AudioView>
+                    <input 
+                        type="range"
+                        name="audio"
+                        min={0}
+                        max={1}
+                        step={ .1 } 
+                        onChange={ (e) => setAudioLevel(e.target.value) }
+                    />
+                </AudioView>
+
+                <DifficultyView>
+                    <Difficulty onClick={ this._setDifficulty.bind(this, EASY) }>Easy</Difficulty>
+                    <Difficulty onClick={ this._setDifficulty.bind(this, MEDIUM) }>Medium</Difficulty>
+                    <Difficulty onClick={ this._setDifficulty.bind(this, HARD) }>Hard</Difficulty>
+                    <Difficulty onClick={ this._setDifficulty.bind(this, VERY_HARD) }>You Know the Way</Difficulty>
+                </DifficultyView>
+            </SettingsView>
         )
     }
+
+    _setDifficulty = (difficulty) => {
+        this.props.setDifficulty(difficulty)
+    }
 }
+
+const Difficulty = styled.div``
+const AudioView = styled.div`
+
+`
+const DifficultyView = styled.div`
+
+`
+const SettingsView = styled.div`
+
+`
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ ...settingsActions }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings)
