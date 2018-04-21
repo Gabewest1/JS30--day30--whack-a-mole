c008d27dd95018687a15e755aabc00fb0e28b008
Gabriel West <gabe_west1@yahoo.com>
2018-04-20 22:11:46 -0500

    Added resetGame action

--- src/redux/WhackAMole/actions.js
+++ src/redux/WhackAMole/actions.js
@@ -6,3 +6,4 @@ export const deactivateMole = mole => ({ type: constants.DEACTIVATE_MOLE, payloa
 export const increaseScore = () => ({ type: constants.INCREASE_SCORE })
 export const decreaseTimer = () => ({ type: constants.TICK })
 export const startGame = () => ({ type: constants.START_GAME })
+export const resetGame = () => ({ type: constants.RESET_GAME })
